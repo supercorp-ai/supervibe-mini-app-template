5369651ce2b4002b5049a3f3f33f1658bd4333bf
Domas <domas.bitvinskas@me.com>
2025-02-09 19:47:47 +0200

    Pass global errors

---
+++ src/app/global-error.tsx
@@ -0,0 +1,41 @@
+'use client'
+import { useEffect } from 'react'
+import { Typography, Button } from '@worldcoin/mini-apps-ui-kit-react'
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    window.parent.postMessage(
+      {
+        type: 'iframe-global-error',
+        message: JSON.stringify({
+          message: error.message,
+          stack: error.stack,
+          name: error.name,
+          digest: error.digest,
+        }),
+      },
+      '*'
+    )
+  }, [error])
+
+  return (
+    <html>
+      <body>
+        <Typography variant="heading">
+          An error occurred
+        </Typography>
+        <Button
+          onClick={() => reset()}
+        >
+          Try again
+        </Button>
+      </body>
+    </html>
+  )
+}
