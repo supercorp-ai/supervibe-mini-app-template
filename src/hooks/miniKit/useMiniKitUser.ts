5d01135048daa573ee545dad286719ae3e293ac3
Domas <domas.bitvinskas@me.com>
2025-02-15 19:01:44 +0200

    Other mini kit user setup

--- src/hooks/miniKit/useMiniKitUser.ts
+++ src/hooks/miniKit/useMiniKitUser.ts
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react"
-import { MiniKit } from '@worldcoin/minikit-js';
+
+const getMiniKitUser = () => {
+  if (typeof window === 'undefined') return null
+  if (!window.MiniKit) return null
+
+  return window.MiniKit.user ?? null
+}
 
 export const useMiniKitUser = () => {
-  const [miniKitUser, setMiniKitUser] = useState(MiniKit.user ?? null);
+  const [miniKitUser, setMiniKitUser] = useState(getMiniKitUser());
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setMiniKitUser(MiniKit.user ?? null);
+      setMiniKitUser(getMiniKitUser());
     }, 500)
 
     return () => clearInterval(interval);
