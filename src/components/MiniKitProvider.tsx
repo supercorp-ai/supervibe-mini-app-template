17ccf3231848a612bcb1cb99bd9d35af1604af94
Domas <domas.bitvinskas@me.com>
2025-02-16 07:27:35 +0200

    Install before

--- src/components/MiniKitProvider.tsx
+++ src/components/MiniKitProvider.tsx
@@ -1,13 +1,20 @@
 'use client';
 
 import { MiniKit } from '@worldcoin/minikit-js';
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 export const MiniKitProvider = ({ children }: { children: ReactNode }) => {
+  const [ready, setReady] = useState(false);
+
   useEffect(() => {
     MiniKit.install();
     console.log(MiniKit.isInstalled());
+    setReady(true);
   }, []);
+
+  if (!ready) {
+    return null;
+  }
 
   return <>{children}</>;
 };
